056936ad4681e99db0259e57193733c667810737
Benjamin Coates <benjcoates@gmail.com>
2017-05-04 18:03:37 -0400

    no message

--- craft/public/resources/js/custom.js
+++ craft/public/resources/js/custom.js
@@ -1,21 +1,13 @@
-// $(document).on('click', '.menu-toggle', function() {
-//     $(this).find(".menu-bar").toggleClass('menu-active');
-// });
-
-// $('.carousel').carousel({
-//   interval: false;
-// });
-
-// $(document).ready(function() {  
-// 	$("#annoucementsCarousel").swiperight(function() {  
-// 	    $("#annoucementsCarousel").carousel('prev');  
-// 	});  
-// 	$("#annoucementsCarousel").swipeleft(function() {  
-// 	    $("#annoucementsCarousel").carousel('next');  
-// 	});  
-// });  
-
-// Used on the Historical Data Page to change the title of the dropdown according to the content
+ // Used on the Historical Data Page to change the title of the dropdown according to the content
 $('.option').on("click", function(event) {
   $('.historical-data a.dropdown-toggle').text($(event.target).text()).append('&nbsp;<span class="caret"></span>');
+});
+
+// Scroll to a certain element
+$("#scroll-down").click(function () {
+	event.preventDefault();
+
+	$('html, body').animate({
+        scrollTop: $("#scroll-down").offset().top
+    }, 500);
 });
